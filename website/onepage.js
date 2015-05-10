f2f386573be0d411c6e1e8ff816df8bb62b566a6
rndethan <EC005@VHMSSTS-01.atcltd.atc.org.nz>
2015-05-11 10:46:35 +1200

    Updated CSS

--- website/onepage.js
+++ website/onepage.js
@@ -1,3 +1,5 @@
+
+// Resizes divs.
 $(document).ready(function() {
   $(window).resize(function() {
     $('.row').height($(window).height());
@@ -5,6 +7,23 @@ $(document).ready(function() {
 });
 
 // Handle clicks.
+$(document).on('click', '#topdiv', function() {
+  $('html, body').animate({
+       scrollTop: $('#top').offset().top
+     }, 750);
+
+     return false;
+});
+
+$(document).on('click', '#middlediv', function() {
+  $('html, body').animate({
+       scrollTop: $('#middle').offset().top
+     }, 750);
+
+     return false;
+});
+
+
 $(document).on('click', '#bottomdiv', function() {
   $('html, body').animate({
        scrollTop: $('#bottom').offset().top
@@ -12,65 +31,3 @@ $(document).on('click', '#bottomdiv', function() {
 
      return false;
 });
-
-/*
-var distance = $('div').offset().top,
-    $window = $(window);
-
-$window.scroll(function() {
-    if ( $window.scrollTop() >= distance ) {
-        // Your div has reached the top
-    }
-});
-*/
-
-/*$('.bottom').on('click', function () {
-    var ele = $(this).closest("body").find(".row yellow");
-    $("body").animate({
-         scrollTop: $(ele).offset().top
-                    }, 100);
-    return false;
-});*/
-
-/*jQuery(function($) {
-
-  var $sections = $('.row'),
-    $animContainer = $('html, body'),
-    $document = $(document),
-    numSections = $sections.length,
-    currSection = 0,
-    isAnimating = false;
-
-  // Animate to a specific index.
-  var gotoSection = function(index) {
-    isAnimating = true;
-    $animContainer.animate({
-      scrollTop: $sections.eq(index).offset().top
-    }, 750, function () {
-      isAnimating = false;
-    });
-  };
-
-  // Advance to next or previous section.
-  var handleAction = function(direction) {
-    if (isAnimating) { return false; }
-
-    if (direction === 'prev' && currSection > 0) { currSection--; }
-    else if (direction === 'next' && currSection < numSections - 1) { currSection++; }
-    else { return false; } // This shouldn't happen.
-
-    gotoSection(currSection);
-  };
-
-  // Handle clicks.
-  $document.on('click', '.action', function() {
-    handleAction($(this).data('direction'));
-  });
-
-  // Handle keyboard input.
-  $document.keyup(function(e){
-    if (e.keyCode === 38) { handleAction('prev'); } // Up arrow.
-    if (e.keyCode === 40) { handleAction('next'); } // Down arrow.
-  });
-
-});*/
